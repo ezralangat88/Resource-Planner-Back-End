a9afdc9da2f0fa6483db98e74c244bfdcff65fb9
Ezra <ezra.banalyst@gmail.com>
2022-01-23 19:27:50 +0300

    basic CRUD

---
+++ src/main/java/com/react_spring_boot/User/User.java
@@ -0,0 +1,66 @@
+package com.react_spring_boot.User;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+    /** Default Constructor */
+    public User() {
+    }
+
+    /** All Arguments Constructor */
+    public User(int id, String firstName, String lastName, String emailId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    /** Getters and Setters */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
---
+++ src/main/java/com/react_spring_boot/User/UserController.java
@@ -0,0 +1,82 @@
+package com.react_spring_boot.User;
+
+import com.react_spring_boot.Exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    /** READ or LIST*/
+    @GetMapping("/user")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    /** READ / Find By ID */
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getEmployeeById(@PathVariable(value = "id") Integer id)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("user not found for this id :: " + id));
+        return ResponseEntity.ok().body(user);
+    }
+
+    /** CREATE / SAVE  */
+    // @RequestBody Uses http message convertors to convert json message into java object
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    /** UPDATE */
+    // @PathVariable is used in Binding url value to method parameter value
+    @PutMapping("/user/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Integer id,
+                                           @RequestBody User userDetails) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("user not found for this id :: " + id));
+
+        user.setEmailId(userDetails.getEmailId());
+        user.setLastName(userDetails.getLastName());
+        user.setFirstName(userDetails.getFirstName());
+
+        User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    /** DELETE */
+    @DeleteMapping("/user/{id}")
+    public Map<String, Boolean> deleteUser1(@PathVariable(value = "id") Integer id)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + id));
+
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+    //    /** DELETE 2 */
+//    @DeleteMapping("/user/{id}")
+//    public ResponseEntity<?> deleteUser(@PathVariable(value = "id") Integer id)
+//            throws ResourceNotFoundException {
+//        userRepository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException("user not found for this id :: " + id));
+//
+//        userRepository.deleteById(id);
+//
+//        return ResponseEntity.ok().build();
+//    }
+}
